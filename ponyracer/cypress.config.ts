8a4c8aa079d86fadc2a59c07d416e616255dded0
luismotv <luismotv77@gmail.com>
2022-08-22 14:00:39 -0500

    15 files modified, 3 added, 1 deleted

--- ponyracer/cypress.config.ts
+++ ponyracer/cypress.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    'baseUrl': 'http://localhost:4200',
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'results/mochawesome',
+    overwrite: false,
+    html: false,
+    json: true,
   },
-})
+  video: false,
+  e2e: {
+    baseUrl: 'http://localhost:4201',
+  },
+});
--- ponyracer/src/app/app.component.spec.ts
+++ ponyracer/src/app/app.component.spec.ts
@@ -1,31 +1,20 @@
 import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    })
+  );
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ponyracer'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ponyracer');
-  });
-
-  it('should render title', () => {
+  it('should have a title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ponyracer app is running!');
+    const element = fixture.nativeElement;
+    expect(element.querySelector('h1').textContent)
+      .withContext('You should have an `h1` with the text Ponyracer')
+      .toContain('Ponyracer');
   });
 });
